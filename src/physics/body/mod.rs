6977ffc0bbf12733bbd4234d674405a8dd531e34
michaelimmanuel <michael.mimo24@gmail.com>
2026-01-08 10:32:59 +0700

    added physics body and applied gravity

---
+++ src/physics/body/mod.rs
@@ -0,0 +1,48 @@
+pub mod test;// use crate::math::Vec2;
+use crate::math::vec2::Vec2;
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+
+pub struct Body {
+    pub mass: f32,
+    pub inv_mass: f32,
+    pub velocity: Vec2,
+    pub position: Vec2,
+    pub force: Vec2,
+}
+
+impl Body {
+
+    const GRAVITY: Vec2 = Vec2 { x: 0.0, y: 9.81 };
+
+    pub fn new(mass: f32, position: Vec2) -> Self {
+        let inv_mass = if mass != 0.0 { 1.0 / mass } else { 0.0 };
+        Self {
+            mass,
+            inv_mass,
+            velocity: Vec2::new(0.0, 0.0),
+            position,
+            force: Vec2::new(0.0, 0.0),
+        }
+    }
+
+    pub fn apply_force(&mut self, force: Vec2) {
+        self.force = self.force + force;
+    }
+
+    pub fn integrate(&mut self, dt: f32) {
+        if self.inv_mass == 0.0 {
+            return;
+        }
+
+        self.apply_force(Self::GRAVITY * self.mass);
+
+        let acceleration = self.force * self.inv_mass;
+        self.velocity = self.velocity + acceleration * dt;
+
+        self.position = self.position + self.velocity * dt;
+
+        self.force = Vec2::new(0.0, 0.0);
+    }
+
+}
